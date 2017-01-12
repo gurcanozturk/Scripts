cdec65bc64044ac966a50b8bd8b7f9336b0556e7
gurcanozturk <github@gurcanozturk.com>
2017-01-12 11:19:20 +0300

    Create convertMaxmindDB.py
    Citrix NetScaler ADC able to use Maxmind GeoLocation DB file for geolocation queries. But character count of each line in the DB file should be limited with 32.
    
    Use this script to convert to 32 char lines in the file

---
+++ convertMaxmindDB.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Truncates ISP field to 30 chars in Maxmind's GeoIPISP DB file (GeoIP-142)
+# to use for NetScaler GeoIP check mechanism.
+# A. Gurcan OZTURK, 09/2015
+
+import sys
+import csv
+import re
+import string
+import argparse
+
+parser = argparse.ArgumentParser(usage='%(prog)s -i <input_file> -o <output_file> [-h]')
+parser.add_argument('-i','--input-file', help='GeoIP DB file to process', required=True)
+parser.add_argument('-o','--output-file', help='GeoIP DB output file', required=True)
+
+if len(sys.argv) < 2:
+  parser.print_usage()
+  sys.exit(1)
+else:
+  args = parser.parse_args()
+
+input_file  = args.input_file
+output_file = args.output_file
+
+ofile  = open(output_file, "w")
+writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+count = 0
+array = []
+with open(input_file, 'rb') as f:
+    reader = csv.reader(f, delimiter=',')
+
+    line_count = 0
+    for row in reader:
+        count +=1
+        m = re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', row[0])
+
+        if m is not None:
+            beginIP  = row[0]
+            finishIP = row[1]
+            country  = row[2]
+            isp      = row[3][:30]
+
+
+            writer.writerow((beginIP,finishIP,country,isp))
+            line_count += 1
+        else:
+            array.append(row)
+
+ofile.close()
+
+if count != line_count:
+    print "Input file %s file has %s lines" % (input_file, count)
+    print "Output file %s file has %s lines" % (output_file, line_count)
+    print "\r"
+    print "Excluded lines:"
+    for line in array:
+        print line
