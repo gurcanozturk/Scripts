4c8abfb9ea9a5da2ac1e5226ec6e498f9dd579da
A.Gurcan Ozturk <github@gurcanozturk.com>
2018-08-27 09:32:36 +0300

    Create delmail.py

---
+++ delmail.py
@@ -0,0 +1,15 @@
+import imaplib
+imap_server = "mail.acme.com"
+imap_user   = "gurcanozturk"
+imap_pass   = ""
+subject     = "Delete Me"
+box = imaplib.IMAP4_SSL(imap_server, 993)
+box.login(imap_user, imap_pass)
+box.select('Inbox')
+typ, data = box.search(None,'(UNSEEN SUBJECT "%s")' % subject)
+for num in data[0].split():
+   print num
+   box.store(num, '+FLAGS', '\\Deleted')
+box.expunge()
+box.close()
+box.logout()
