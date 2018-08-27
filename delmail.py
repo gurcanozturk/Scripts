6afd2bae0a0eed3ae9ef4aa7e0b0d7a13a1f4b07
A.Gurcan Ozturk <github@gurcanozturk.com>
2018-08-27 09:33:29 +0300

    Update delmail.py

--- delmail.py
+++ delmail.py
@@ -1,8 +1,11 @@
+# Delete mails from IMAP server by subject.
+#
 import imaplib
 imap_server = "mail.acme.com"
 imap_user   = "gurcanozturk"
 imap_pass   = ""
 subject     = "Delete Me"
+
 box = imaplib.IMAP4_SSL(imap_server, 993)
 box.login(imap_user, imap_pass)
 box.select('Inbox')
